1acdb495fa11670a9b847825c3abd599308d2d42
Flarp <flurpdadurp@gmail.com>
2024-03-19 23:06:36 -0400

    Unfinished state, but synchronizing

--- src/pages/Graph.tsx
+++ src/pages/Graph.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useReducer } from "react"
 import { SigmaContainer } from "@react-sigma/core";
 import "@react-sigma/core/lib/react-sigma.min.css";
 import { KnowledgeGraph, Topic } from "../api/model"
@@ -20,9 +20,45 @@ const SUBJECT_COLORS: { [key: string]: string } = {
     "Analysis": "yellow"
 }
 
+type GraphReduceAction = {
+    type: "deleteNode",
+    node: number 
+} | {
+    type: "addNode"
+} | {
+    type: "addEdge",
+    source: number,
+    destination: number
+} | {
+    type: "removeEdge",
+    source: number,
+    destination: number
+}
+
+// unfinished!
+const graphReducer = (state: KnowledgeGraph, action: GraphReduceAction): KnowledgeGraph => {
+    if (action.type === "deleteNode") {
+        state.topics = state.topics
+            .filter(topic => topic.id != action.node)
+            .map(topic => {
+                topic.requirements = topic.requirements.filter(requirement => requirement !== action.node)
+                return topic
+            })
+
+        return state
+    } else if (action.type === "removeEdge") {
+        const index = state.topics.findIndex(topic => topic.id == action.destination)
+
+        return state
+    } else {
+        return state
+    }
+}
+
 // The nodes and edges selected within the graph are internal state.
 const Graph = (props: GraphProps) => {
-    const [graph, setGraph] = useState(props.graph)
+    const [graph, dispatch] = useReducer(graphReducer, props.graph)
+
     const [topics, setTopics] = useState(new Set())
     const [openedTopic, setOpenTopic]: [Topic | null, any] = useState(null)
 
@@ -42,13 +78,17 @@ const Graph = (props: GraphProps) => {
         }
     }
 
+    const modifyTopic = (id: number) => {
+
+    }
+
     const closeTopic = () => setOpenTopic(null)
 
     return <div>
         <SearchBar topics={graph.topics} />
         <SigmaContainer style={{ height: "90vh", width: "100vw" }}>
             <DagGraph graph={graph}/>
-            <GraphViewEvents clickTopic={openTopic}/>
+            <GraphEditEvents selectTopic={selectTopic} modifyTopic={modifyTopic}/>
         </SigmaContainer>
         <GraphEditor graph={graph} setGraph={setGraph} />
         
