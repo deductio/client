01df43308d17944d0da60f10bbf6f20c1734e0ce
Flarp <flurpdadurp@gmail.com>
2024-06-08 19:07:24 -0400

    Properly mark deleted files

--- src/pages/NotFound.tsx
+++
@@ -1,7 +0,0 @@
-const NotFound = () => {
-    return <div>
-        <h1 className="text-2xl p-4">The page you're looking for isn't found; did you make a typo?</h1>
-    </div>
-}
-
-export default NotFound
